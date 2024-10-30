d5251e1d0fd5b8f2797ece84a894e12c4a22a8ce
hasancse18 <hasan7779296@gmail.com>
2024-10-30 21:26:14 +0600

    remove mongo url

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const mongoUrl = process.env.DB_URL;
-//const mongoUrl = 'mongodb://localhost:27017/mydb';
-//const mongoUrl = 'mongodb+srv://hasancse:hasancse@cluster0.ckt9f.mongodb.net/' 
+ 
 mongoose.connect(mongoUrl)
 
 const db = mongoose.connection;
