5daf730eadcc3d6be6b95b90223677a6be793448
hasancse18 <hasan7779296@gmail.com>
2024-11-03 20:39:09 +0600

    Add MiddleWare

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
-
+const logRequest = (req,res,next)=>{
+    console.log(`[${new Date().toLocaleDateString()}] Request Made to : ${req.orginalUrl}`)
+    next();
+}
+app.use(logRequest);
 
 
 //import person data
@@ -13,11 +17,13 @@ const prouter = require('./routes/personRoutes')
 //import menu data
 const mrouter = require('./routes/menuRoutes')
 
-
 app.use('/person',prouter)
 app.use('/menu',mrouter)
 
 const port = process.env.PORT || 3000;
+//MiddleWare Function
+
+
 app.listen(port,()=>{
     console.log("Listening on port 3000")
 })
