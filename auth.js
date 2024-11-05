1d051163be20a8e860e833633c71b22998669dc0
hasancse18 <hasan7779296@gmail.com>
2024-11-05 21:29:47 +0600

    Add Authentication

---
+++ auth.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./Models/person')
+passport.use(new LocalStrategy(async (uname, pass, done)=>{
+    //authentication
+    try {
+        console.log('Received Credentials: ', uname, pass);
+        const user = await Person.findOne({username: uname});
+        if(!user)
+            return done(null, false,{message: 'Incorrect username'})
+        const isMatch = user.password===pass? true:false;
+        if(isMatch)
+            return done(null,user);
+        else
+            return done(null,false, {message: 'Incorrect password.'})
+    } catch (error) {
+        return done(error);
+    }
+}))
+
+
+module.exports = passport
--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const express = require('express');
 const app = express();
 const db = require('./db')
 require('dotenv').config();
+const passport = require('./auth');
+
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
-
+//MiddleWare Function
 const logRequest = (req,res,next)=>{
     console.log(`[${new Date().toLocaleDateString()}] Request Made to : ${req.orginalUrl}`)
     next();
 }
 app.use(logRequest);
 
+
+app.use(passport.initialize())
+const middleAuthenticate = passport.authenticate('local',{session:false});
+
+app.get('/',middleAuthenticate,(req,res)=>{
+    res.send('welcome to our hotel');
+})
 
 //import person data
 const prouter = require('./routes/personRoutes')
@@ -21,7 +31,7 @@ app.use('/person',prouter)
 app.use('/menu',mrouter)
 
 const port = process.env.PORT || 3000;
-//MiddleWare Function
+
 
 
 app.listen(port,()=>{
